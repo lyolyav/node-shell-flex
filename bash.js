aec8f38e73984eba30cf950780403e615ecae442
Olga Matyukhina <matyukhinaolga@gmail.com>
2022-07-19 21:10:36 -0400

    Olgas first edit

--- bash.js
+++ bash.js
@@ -2,10 +2,8 @@
 process.stdout.write('prompt > ');
 
 // The stdin 'data' event fires after a user types in a line
-process.stdin.on('data', (data) => {
-  const cmd = data.toString().trim(); //remove the newline
-  if(cmd === 'pwd'){
-    process.stdout.write(process.cwd());
-  }
-  process.stdout.write('\nprompt > ');
-});
+
+const pwd = require('./pwd');
+pwd();  // what difference does it make invoking the function here or not
+
+
